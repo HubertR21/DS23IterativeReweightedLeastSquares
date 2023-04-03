631fb7ef1cc779ade1d4f5649d307f9d4c805732
HubertR21 <hruczynski21@interia.pl>
2023-04-03 18:16:26 +0200

    update with vignette

---
+++ R/data_banknote.R
@@ -0,0 +1,25 @@
+#' Banknote dataset
+#'
+#' Data were extracted from images that were taken from genuine and forged
+#' banknote-like specimens. For digitization, an industrial camera usually
+#' used for print inspection was used. The final images have 400x 400 pixels.
+#' Due to the object lens and distance to the investigated object gray-scale
+#' pictures with a resolution of about 660 dpi were gained. Wavelet Transform
+#' tool were used to extract features from images.
+#'
+#'
+#' @format A data frame with 1372 rows and 5 variables:
+#' \describe{
+#'   \item{X1}{numeric, variance of Wavelet Transformed image}
+#'   \item{X2}{numeric, skewness of Wavelet Transformed image }
+#'   \item{X3}{numeric, curtosis of Wavelet Transformed image}
+#'   \item{X4}{numeric, ntropy of image}
+#'   \item{y}{integer, class}
+#' }
+#' @name banknote
+#' @docType data
+#' @usage data(banknote)
+#'
+#' @source Data from UCL \url{https://archive.ics.uci.edu/ml/datasets/banknote+authentication}
+#'
+NULL
---
+++ R/data_raisin.R
@@ -0,0 +1,27 @@
+#' Raisin dataset
+#'
+#' Images of Kecimen and Besni raisin varieties grown in Turkey were obtained
+#' with CVS. A total of 900 raisin grains were used, including 450 pieces from
+#' both varieties. These images were subjected to various stages of pre-processing
+#' and 7 morphological features were extracted. These features have been classified
+#' using three different artificial intelligence techniques.
+#'
+#'
+#' @format A data frame with 900 rows and 8 variables:
+#' \describe{
+#'   \item{Area}{integer, Gives the number of pixels within the boundaries of the raisin.}
+#'   \item{Perimeter}{numeric, It measures the environment by calculating the distance between the boundaries of the raisin and the pixels around it.}
+#'   \item{MajorAxisLength}{numeric, Gives the length of the main axis, which is the longest line that can be drawn on the raisin.}
+#'   \item{MinorAxisLength}{numeric, Gives the length of the small axis, which is the shortest line that can be drawn on the raisin.}
+#'   \item{Eccentricity}{numeric, It gives a measure of the eccentricity of the ellipse, which has the same moments as raisins.}
+#'   \item{ConvexArea}{integer, Gives the number of pixels of the smallest convex shell of the region formed by the raisin.}
+#'   \item{Extent}{numeric, Gives the ratio of the region formed by the raisin to the total pixels in the bounding box.}
+#'   \item{Class}{integer, Kecimen and Besni raisin.}
+#' }
+#' @name raisin
+#' @docType data
+#' @usage data(raisin)
+#'
+#' @source Data from UCL \url{https://archive.ics.uci.edu/ml/datasets/Raisin+Dataset}
+#'
+NULL
